0181d8c15f41051b8b7e2a90da55988bf62bb364
Jonathan Warner <jaxbot@gmail.com>
2015-04-15 13:29:22 -0400

    Terminal support, slide fixes

--- slides.js
+++ slides.js
@@ -1,9 +1,37 @@
 var currentSlide = 1;
 
+var socket = io.connect("http://localhost:9999/");
+
 function nextSlide() {
         if (currentSlide < 1)
                 currentSlide = 1;
-        document.getElementById("slide").innerHTML = document.getElementById("slide" + currentSlide).innerHTML;
+
+        document.getElementById("slide").innerHTML = document.querySelectorAll(".slide")[currentSlide].innerHTML;
+
+        location.hash = "#" + currentSlide;
+
+        var termContainer = document.querySelector("#slide .terminal-container");
+        if (termContainer) {
+                console.log("found term");
+                socket.emit("newvim", { file: termContainer.getAttribute("data-file") });
+                var term = new Terminal({
+                        cols: 80,
+                        rows: 24,
+                        useStyle: true,
+                        screenKeys: true,
+                        cursorBlink: false
+                });
+                term.on('data', function(data) {
+                        socket.emit('data', data);
+                });
+                term.open(termContainer);
+                socket.on('data', function(data) {
+                        term.write(data);
+                });
+                socket.on('disconnect', function() {
+                        term.destroy();
+                });
+        }
 }
 
 window.onload = nextSlide;
@@ -17,4 +45,8 @@ window.addEventListener("keyup", function(event) {
         }
         console.log(event.keyCode);
 });
+
+if (location.hash) {
+        currentSlide = ~~location.hash.substring(1);
+}
 
